87e088e72dcdc2401045a02a2394cc088ff42db1
masood <masoodacheampong@gmail.com>
2025-02-23 09:54:41 +0000

    installed compression package for tailwindcss performance

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
 const authorsRouter = require("./routes/author");
 const bodyParser = require("body-parser");
+const compression = require("compression");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -14,6 +15,7 @@ app.set("layout", "layouts/layout");
 app.use(expressLayout);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use(compression());
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
